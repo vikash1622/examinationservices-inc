6df046b96311035eb37f071a23db643a023521af
vikash1622 <vy9983091105@gmail.com>
2025-08-20 07:28:05 +0530

    first commit

---
+++ src/componentnts/PercentileTable.jsx
@@ -0,0 +1,107 @@
+import React from 'react'
+
+const PercentileTable = () => {
+    return (
+        <table style={{ borderCollapse: 'collapse', marginLeft: '3pt', marginTop: '10px' }} cellSpacing="0">
+            <tbody>
+                <tr style={{ height: '18pt' }}>
+                    <td style={{ width: '575pt', border: '1pt solid #CCCCCC' }} colSpan="5">
+                        <p className="s3" style={{ paddingTop: '1pt', paddingLeft: '1pt', textIndent: '0pt', textAlign: 'center' }}>Percentile obtained</p>
+                    </td>
+                </tr>
+                <tr style={{ height: '16pt' }}>
+                    <td style={{ width: '201pt', border: '1pt solid #CCCCCC' }}>
+                        <p className="s1" style={{ paddingTop: '1pt', paddingLeft: '1pt', textIndent: '0pt', textAlign: 'left' }}>Physics</p>
+                    </td>
+                    <td style={{ width: '374pt', border: '1pt solid #CCCCCC' }} colSpan="4">
+                        <p className="s2" style={{ paddingTop: '2pt', paddingLeft: '1pt', textIndent: '0pt', textAlign: 'left' }}>92.9041446</p>
+                    </td>
+                </tr>
+                <tr style={{ height: '15pt' }}>
+                    <td style={{ width: '201pt', border: '1pt solid #CCCCCC' }}>
+                        <p className="s1" style={{ paddingTop: '1pt', paddingLeft: '1pt', textIndent: '0pt', textAlign: 'left' }}>Chemistry</p>
+                    </td>
+                    <td style={{ width: '374pt', border: '1pt solid #CCCCCC' }} colSpan="4">
+                        <p className="s2" style={{ paddingTop: '2pt', paddingLeft: '1pt', textIndent: '0pt', textAlign: 'left' }}>97.1646183</p>
+                    </td>
+                </tr>
+                <tr style={{ height: '16pt' }}>
+                    <td style={{ width: '201pt', border: '1pt solid #CCCCCC' }}>
+                        <p className="s1" style={{ paddingTop: '1pt', paddingLeft: '1pt', textIndent: '0pt', textAlign: 'left' }}>Biology (Botany & Zoology)</p>
+                    </td>
+                    <td style={{ width: '374pt', border: '1pt solid #CCCCCC' }} colSpan="4">
+                        <p className="s2" style={{ paddingTop: '2pt', paddingLeft: '1pt', textIndent: '0pt', textAlign: 'left' }}>98.4473487</p>
+                    </td>
+                </tr>
+                <tr style={{ height: '15pt' }}>
+                    <td style={{ width: '201pt', border: '1pt solid #CCCCCC' }}>
+                        <p className="s1" style={{ paddingTop: '1pt', paddingLeft: '1pt', textIndent: '0pt', textAlign: 'left' }}>Percentile (Based on total marks obtained)</p>
+                    </td>
+                    <td style={{ width: '374pt', border: '1pt solid #CCCCCC' }} colSpan="4">
+                        <p className="s2" style={{ paddingTop: '2pt', paddingLeft: '1pt', textIndent: '0pt', textAlign: 'left' }}>94.0102077</p>
+                    </td>
+                </tr>
+                <tr style={{ height: '16pt' }}>
+                    <td style={{ width: '201pt', border: '1pt solid #CCCCCC' }}>
+                        <p className="s1" style={{ paddingTop: '1pt', paddingLeft: '1pt', textIndent: '0pt', textAlign: 'left' }}>Percentile (Based on total marks obtained) in words</p>
+                    </td>
+                    <td style={{ width: '374pt', border: '1pt solid #CCCCCC' }} colSpan="4">
+                        <p className="s2" style={{ paddingTop: '2pt', paddingLeft: '1pt', textIndent: '0pt', textAlign: 'left' }}>Ninety Four  point Zero One Zero Two Zero Seven Seven Only</p>
+                    </td>
+                </tr>
+                <tr style={{ height: '15pt' }}>
+                    <td style={{ width: '201pt', border: '1pt solid #CCCCCC' }}>
+                        <p className="s1" style={{ paddingTop: '1pt', paddingLeft: '1pt', textIndent: '0pt', textAlign: 'left' }}>Total marks obtained (out of 720)</p>
+                    </td>
+                    <td style={{ width: '374pt', border: '1pt solid #CCCCCC' }} colSpan="4">
+                        <p className="s2" style={{ paddingTop: '2pt', paddingLeft: '1pt', textIndent: '0pt', textAlign: 'left' }}>507</p>
+                    </td>
+                </tr>
+                <tr style={{ height: '16pt' }}>
+                    <td style={{ width: '201pt', border: '1pt solid #CCCCCC' }}>
+                        <p className="s1" style={{ paddingTop: '1pt', paddingLeft: '1pt', textIndent: '0pt', textAlign: 'left' }}>Total marks obtained (out of 720) in words</p>
+                    </td>
+                    <td style={{ width: '374pt', border: '1pt solid #CCCCCC' }} colSpan="4">
+                        <p className="s2" style={{ paddingTop: '2pt', paddingLeft: '1pt', textIndent: '0pt', textAlign: 'left' }}>Five Hundred Seven Only</p>
+                    </td>
+                </tr>
+                <tr style={{ height: '15pt' }}>
+                    <td style={{ width: '201pt', border: '1pt solid #CCCCCC' }}>
+                        <p className="s1" style={{ paddingTop: '1pt', paddingLeft: '1pt', textIndent: '0pt', textAlign: 'left' }}>NEET All-India Rank</p>
+                    </td>
+                    <td style={{ width: '70pt', border: '1pt solid #CCCCCC' }}>
+                        <p className="s1" style={{ paddingTop: '1pt', paddingLeft: '1pt', textIndent: '0pt', textAlign: 'left' }}>Category</p>
+                    </td>
+                    <td style={{ width: '104pt', border: '1pt solid #CCCCCC' }}>
+                        <p className="s1" style={{ paddingTop: '1pt', paddingLeft: '2pt', textIndent: '0pt', textAlign: 'left' }}>Category rank</p>
+                    </td>
+                    <td style={{ width: '48pt', border: '1pt solid #CCCCCC' }}>
+                        <p className="s1" style={{ paddingTop: '1pt', paddingLeft: '2pt', textIndent: '0pt', textAlign: 'left' }}>PwBD</p>
+                    </td>
+                    <td style={{ width: '152pt', border: '1pt solid #CCCCCC' }}>
+                        <p className="s1" style={{ paddingTop: '1pt', paddingLeft: '2pt', textIndent: '0pt', textAlign: 'left' }}>Category-PwBD rank</p>
+                    </td>
+                </tr>
+                <tr style={{ height: '16pt' }}>
+                    <td style={{ width: '201pt', border: '1pt solid #CCCCCC' }}>
+                        <p className="s2" style={{ paddingTop: '2pt', paddingLeft: '1pt', textIndent: '0pt', textAlign: 'left' }}>41432 </p>
+                    </td>
+                    <td style={{ width: '70pt', border: '1pt solid #CCCCCC' }}>
+                        <p className="s2" style={{ paddingTop: '2pt', paddingLeft: '1pt', textIndent: '0pt', textAlign: 'left' }}>	OBC- NCL (Central List)</p>
+                    </td>
+                    <td style={{ width: '104pt', border: '1pt solid #CCCCCC' }}>
+                        <p className="s2" style={{ paddingTop: '2pt', paddingLeft: '2pt', textIndent: '0pt', textAlign: 'left' }}>16356 </p>
+                    </td>
+                    <td style={{ width: '48pt', border: '1pt solid #CCCCCC' }}>
+                        <p className="s2" style={{ paddingTop: '2pt', paddingLeft: '2pt', textIndent: '0pt', textAlign: 'left' }}>NO</p>
+                    </td>
+                    <td style={{ width: '152pt', border: '1pt solid #CCCCCC' }}>
+                        <p className="s2" style={{ paddingTop: '2pt', paddingLeft: '2pt', textIndent: '0pt', textAlign: 'left' }}>----</p>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    );
+}
+
+export default PercentileTable
